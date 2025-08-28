9434d838b5a79d2d8b7b07112866921837185ef3
alex-diasritter <ritter.alex@hotmail.com>
2025-08-28 20:13:02 -0300

    fix: corrigindo nome de fila e add propostsaEmAnalise

--- src/main/java/com/alex/analise_credito/listener/PropostaEmAnaliseListener.java
+++ src/main/java/com/alex/analise_credito/listener/PropostaEmAnaliseListener.java
@@ -1,14 +1,18 @@
 package com.alex.analise_credito.listener;
-
 import com.alex.analise_credito.domain.Proposta;
+import com.alex.analise_credito.service.strategy.AnaliseCreditoService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class PropostaEmAnaliseListener {
 
-    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-    public void propostaEmAnalise(Proposta proposta){
+    @Autowired
+    private AnaliseCreditoService service;
 
+    @RabbitListener(queues = "${rabbitmq.queue.proposta-pendente}")
+    public void propostaEmAnalise(Proposta proposta){
+        service.analisar(proposta);
     }
 }
