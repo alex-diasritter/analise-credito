513683860316b71c67dbac9aa3e78f2a51dd7465
alex-diasritter <ritter.alex@hotmail.com>
2025-08-28 20:09:13 -0300

    feat: enum com mensagens para resposta ao cliente

---
+++ src/main/java/com/alex/analise_credito/constantes/MensagemConstante.java
@@ -0,0 +1,7 @@
+package com.alex.analise_credito.constantes;
+
+public class MensagemConstante {
+
+    public static final String CLIENT_NEGATIVADO = "Operação não permitida. Cliente %s negativado.";
+    public static final String PONTUACAO_SERASA_BAIXA = "Operação não permitida. Cliente com score abaixo de 200 pontos.";
+}
